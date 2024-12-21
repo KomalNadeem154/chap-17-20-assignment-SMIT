5b4cc0a61ff0a702acd4a44afaebb1d4bdf5b142
Komal Nadeem <koomalkhan111@gmail.com>
2024-12-21 21:04:39 +0500

    Add files via upload

---
+++ assignment chap 17-20/app.js
@@ -0,0 +1,117 @@
+// QUESTION NO:1
+
+// let arr = [
+//     [],
+//     [],
+//     []
+// ];
+// console.log(arr);
+
+// QUESTION NO:2
+    //  [
+    //     [1, 2, 3],
+    //     [4, 5, 6],
+    //     [7, 8, 9],
+    //   ]
+
+
+    //   QUESTION NO:3
+
+    // for (let i = 1; i <= 10; i++){
+    //     console.log(i); 
+    // }
+
+    // QUESTION NO:4
+
+    // let table =prompt("enter your table you want")
+    // for ( let i=1 ; i<=10 ; i++){
+    //     document.write(table + "X" + i + "=" + table*i + "<br>")
+    // }
+
+    // QUESTION NO:5
+
+    // let fruits=['apple','banana','mango','orange','strawberry']
+    // console.log(fruits);
+    // for( let i =0; i <length.fruits; i++){
+    //     console.log(fruits(i));
+        
+    // }
+
+    // QUESTION NO:6
+//     document.write("<h2>Counting:</h2>");
+// for (let i = 1; i <= 15; i++) {
+//     document.write(i + ", ");
+// }
+
+// document.write("<br><br>");
+
+
+// document.write("<h2>Reverse Counting:</h2>");
+// for (let i = 10; i >= 1; i--) {
+//     document.write(i + ", ");
+// }
+
+// document.write("<br><br>");
+
+
+// document.write("<h2>Even:</h2>");
+// for (let i = 0; i <= 20; i += 2) {
+//     document.write(i + ", ");
+// }
+
+// document.write("<br><br>");
+
+
+// document.write("<h2>Odd:</h2>");
+// for (let i = 1; i <= 19; i += 2) {
+//     document.write(i + ", ");
+// }
+
+// document.write("<br><br>");
+
+
+// document.write("<h2>Series:</h2>");
+// for (let i = 2; i <= 20; i += 2) {
+//     document.write(i + "k, ");
+// }
+
+// QUESTION NO:7
+
+
+// let bakery = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// let userInput = prompt("Enter an item.");
+
+// if (bakery.includes(userInput.toLowerCase())) {
+
+//     alert(userInput + " is found in the list.");
+// } else {
+
+//     alert(userInput + " is not found in the list.");
+// }
+
+// QUESTION NO:8
+
+// const A = [24, 53, 78, 91, 12];
+
+// const largestNumber = Math.max(...A);
+
+// console.log("The largest number in the array is: " + largestNumber);
+
+// QUESTION NO:9
+
+// const A = [24, 53, 78, 91, 12];
+
+// const largestNumber = Math.min(...A);
+
+// console.log("The Smallest  number in the array is: " + largestNumber);
+
+
+// QUESTION NO:10
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 5 === 0) {
+//         document.write(i + "<br>");
+//     }
+// }
+    
